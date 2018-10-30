06e0348e001b7758b569099d7e5e1b0e99af86cb
choufractal <alice.coucke@gmail.com>
2018-10-30 13:49:58 +0100

    update README

--- dataset.py
+++ dataset.py
@@ -3,14 +3,12 @@
 
 from __future__ import unicode_literals
 
-import logging
+import io
+import json
 import os
 from abc import ABCMeta, abstractproperty, abstractmethod
+from copy import deepcopy
 from pathlib import Path
-
-from utils import load_json, keep_only_utterances_with_audio
-
-logger = logging.getLogger(__name__)
 
 
 class Dataset(object):
@@ -147,3 +145,43 @@ class CrossValDataset(Dataset):
         raise TypeError(
             "CrossValDataset is not meant to be used with train test metrics"
         )
+
+
+def load_json(filename, encoding='utf-8'):
+    """
+        Load the content of filename
+    """
+    with io.open(filename, 'r', encoding=encoding) as _file:
+        return json.load(_file)
+
+
+def keep_only_utterances_with_audio(dataset, audio_corpus):
+    cleaned_up_dataset = deepcopy(dataset)
+    n_skipped = 0
+    total = 0
+    for intent, intent_data in dataset['intents'].iteritems():
+        cleaned_up_dataset['intents'][intent]['utterances'] = []
+        for idx, utt in enumerate(intent_data['utterances']):
+            total += 1
+            sentence = "".join(chunk['text'] for chunk in utt['data'])
+            if sentence in audio_corpus:
+                cleaned_up_dataset['intents'][intent]['utterances'].append(utt)
+            else:
+                n_skipped += 1
+                print(
+                    "Skipping sentence {} from dataset because it does not "
+                    "have an audio file".format(sentence)
+                )
+    print(
+        "{} utterancces skipped out of {}".format(n_skipped, total)
+    )
+
+    return cleaned_up_dataset
+
+
+if __name__ == '__main__':
+    data_dir = "/path/to/smart-lights/data/folder"
+    dataset = TrainTestDataset.from_dir(data_dir)
+    print dataset.get_audio_file(
+        "Set lights to twenty two percent in the basement"
+    )
